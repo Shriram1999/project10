35bb9a9999b233c71f24151a3f615d6232deafbb
Shriram1999 <rpziddi1@gmail.com>
2023-09-27 17:06:34 +0530

    Succes

---
+++ pro10/projectORS/src/main/java/com/rays/dao/SubjectDAOInt.java
@@ -0,0 +1,11 @@
+package com.rays.dao;
+
+import com.rays.common.BaseDAOInt;
+import com.rays.dto.SubjectDTO;
+/**
+ * @author Shriram Patel
+ *
+ */
+public interface SubjectDAOInt extends BaseDAOInt<SubjectDTO>{
+
+}
